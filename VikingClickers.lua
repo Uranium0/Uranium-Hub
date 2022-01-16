7137591524f5e034310e5afb3b8d28e4477a9cce
Uranium0 <97809596+Uranium0@users.noreply.github.com>
2022-01-16 16:58:30 +0100

    Update VikingClickers.lua

--- VikingClickers.lua
+++ VikingClickers.lua
@@ -42,6 +42,81 @@ MainSection:NewToggle("Auto Plunder", "Toggles Auto Plunder on or off", function
         getgenv().AP = false
     end
 end)
+MainSection:NewDropdown("Rebirth Amount", "Pick the amount to auto rebirth", {"1", "10", "100", "1k" , "10k" , "100k"}, function(currentOption)
+    getgenv().RA = currentOption
+end)
+MainSection:NewToggle("Auto Rebirth", "Toggle Auto Rebirth on or off", function(state)
+    if state then
+        getgenv().AR = true
+        while AR == true do
+            wait(0,1)
+            
+            local args = {
+                [1] = RA
+            }
+            
+            game:GetService("ReplicatedStorage").Rebirth:FireServer(unpack(args))
+        end
+      
+    
+    
+    else
+        getgenv().AR = false
+    end
+end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 local Player = Window:NewTab("Player")
+local PlayerSection = Player:NewSection("Movement")
+PlayerSection:NewSlider("Walkspeed", "Changes the walkspeed", 250, 16, function(v) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+end)
+PlayerSection:NewSlider("Jumppover", "Changes the JumpPower", 250, 50, function(v) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
+end)
+
+
+PlayerSection:NewToggle("Infinite Jump", "Makes you jump infinite", function(state)
+    if state then
+        
+        
+        getgenv().infinjump = true
+ 
+        local Player = game:GetService("Players").LocalPlayer
+        local Mouse = Player:GetMouse()
+        Mouse.KeyDown:connect(function(k)
+        if getgenv().infinjump then
+        if k:byte() == 32 then
+        Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+        Humanoid:ChangeState("Jumping")
+        wait(0.1)
+        Humanoid:ChangeState("Seated")
+        end
+        end
+        end)
+ 
+       
+    
+    
+    else
+        getgenv().infinjump = false
+    end
+end)
