a7bc73fb6cffd407da24be79331ba275a3199706
Uranium0 <97809596+Uranium0@users.noreply.github.com>
2022-01-16 16:12:59 +0100

    Update GameSorter.lua

--- GameSorter.lua
+++ GameSorter.lua
@@ -1,6 +1,10 @@
 if game.PlaceId == 7009987220 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Uranium0/Uranium-Hub/main/Ragdollstacks.lua"))()
 
+elseif game.PlaceId == 5444465912 then    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Uranium0/Uranium-Hub/main/VikingClickers.lua"))()
+
+
 else
    
 
